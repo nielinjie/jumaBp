7b4189a428d185331f2c88df41b6054e6f501839
nielinjie <nielinjie@gmail.com>
2019-01-07 10:08:22 +0800

    调用启动process。

--- src/main/java/xyz/nietongxue/jumabp/StartProcess.java
+++ src/main/java/xyz/nietongxue/jumabp/StartProcess.java
@@ -1,21 +1,25 @@
 package xyz.nietongxue.jumabp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Base64;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Executors;
 
 @Configuration
 public class StartProcess {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @MessagingGateway
     public interface ProcessStarter {
@@ -24,50 +28,127 @@ public class StartProcess {
         void placeBE(BE be);
 
     }
-
+    @Bean
+    public RestTemplate rest(){
+        return new RestTemplate();
+    }
     @Bean
     public IntegrationFlow eventStartFlow() {
         return f -> f
                 .channel(c -> c.executor(Executors.newCachedThreadPool()))
                 .handle(m ->
-
+                        startProcess()
                 );
     }
 
+//    @Bean
+//    public MappingJackson2HttpMessageConverter converter(){
+//        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+//        //设置日期格式
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd");
+//        objectMapper.setDateFormat(smt);
+//        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+//        //设置中文编码格式
+//        List<MediaType> list = new ArrayList<MediaType>();
+//        list.add(MediaType.APPLICATION_JSON_UTF8);
+//        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+//        return mappingJackson2HttpMessageConverter;
+//    }
 
+    class StartProcessRequest{
+        String processDefinitionKey;
+        String businessKey;
+        List<Variable> variables;
 
+        public String getProcessDefinitionKey() {
+            return processDefinitionKey;
+        }
 
-    public void testGetEmployeeAsJson() throws Exception{
-        Map<String, Object> employeeSearchMap = getEmployeeSearchMap("0");
+        public void setProcessDefinitionKey(String processDefinitionKey) {
+            this.processDefinitionKey = processDefinitionKey;
+        }
 
-        final String fullUrl = "http://localhost:8080/rest-http/services/employee/{id}/search?format=json";
-        HttpHeaders headers = getHttpHeadersWithUserCredentials(new HttpHeaders());
-        headers.add("Accept", "application/json");
-        HttpEntity<Object> request = new HttpEntity<Object>(headers);
+        public String getBusinessKey() {
+            return businessKey;
+        }
 
-        ResponseEntity<?> httpResponse = restTemplate.exchange(fullUrl, HttpMethod.GET, request, EmployeeList.class, employeeSearchMap);
-        logger.info("Return Status :"+httpResponse.getHeaders().get("X-Return-Status"));
-        logger.info("Return Status Message :"+httpResponse.getHeaders().get("X-Return-Status-Msg"));
-        assertTrue(httpResponse.getStatusCode().equals(HttpStatus.OK));
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        jaxbJacksonObjectMapper.writeValue(out, httpResponse.getBody());
-        logger.info(new String(out.toByteArray()));
+        public void setBusinessKey(String businessKey) {
+            this.businessKey = businessKey;
+        }
+
+        public List<Variable> getVariables() {
+            return variables;
+        }
+
+        public void setVariables(List<Variable> variables) {
+            this.variables = variables;
+        }
+    }
+    class Variable{
+        String name;
+        String value;
+
+        public Variable(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+    public void startProcess() {
+
+        String url = "http://localhost:8080/runtime/process-instances";
+        StartProcessRequest requestJson = new StartProcessRequest();
+        requestJson.setBusinessKey(UUID.randomUUID().toString());
+        requestJson.setProcessDefinitionKey("oneTaskProcess");
+        List<Variable> vs = new ArrayList<>();
+        vs.add(new Variable("myVar","variable value"));
+
+//                "{\n" +
+//                "   \"processDefinitionKey\":\"oneTaskProcess\",\n" +
+//                "   \"businessKey\":\"myBusinessKey\",\n" +
+//                "   \"variables\": [\n" +
+//                "      {\n" +
+//                "        \"name\":\"myVar\",\n" +
+//                "        \"value\":\"This is a variable\"\n" +
+//                "      }\n" +
+//                "   ]\n" +
+//                "}";
+        HttpHeaders headers = new HttpHeaders();
+        getHttpHeadersWithUserCredentials(headers);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<StartProcessRequest> entity = new HttpEntity<>(requestJson, headers);
+        String answer = restTemplate.postForObject(url, entity, String.class);
+        System.out.println(answer);
     }
 
-    private HttpHeaders getHttpHeadersWithUserCredentials(ClientHttpRequest request){
-        return (getHttpHeadersWithUserCredentials(request.getHeaders()));
-    }
 
-    private HttpHeaders getHttpHeadersWithUserCredentials(HttpHeaders headers){
+    private HttpHeaders getHttpHeadersWithUserCredentials(HttpHeaders headers) {
 
-        String username = "SPRING";
-        String password = "spring";
+        String username = "admin";
+        String password = "admin";
 
-        String combinedUsernamePassword = username+":"+password;
+        String combinedUsernamePassword = username + ":" + password;
         byte[] base64Token = Base64.getEncoder().encode(combinedUsernamePassword.getBytes());
-        String base64EncodedToken = new String (base64Token);
+        String base64EncodedToken = new String(base64Token);
         //adding Authorization header for HTTP Basic authentication
-        headers.add("Authorization","Basic  "+base64EncodedToken);
+        headers.add("Authorization", "Basic  " + base64EncodedToken);
 
         return headers;
     }
